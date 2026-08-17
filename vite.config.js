3cb5bac7120f649e00cb931071605887a9338756
ENGDILAR <diloalmio@gmail.com>
2026-08-17 21:30:52 +0300

    Configure GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
+
 export default defineConfig({
+  base: "/ReactPortofolio/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
